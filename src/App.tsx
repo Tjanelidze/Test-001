74666e290120f4e85209d86e5dd201ff1797fe19
t.janelidze <t.janelidze@andersenlab.com>
2025-03-11 11:21:42 +0400

    [FEAT] Implemented fetching of users, Created small UI for showing the result, Implemented interface for usertype

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, List, ListItem, ListItemText } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { UserType } from './types/user.type';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState<UserType[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          'https://jsonplaceholder.typicode.com/users'
+        );
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Box p={3}>
+      <List>
+        {users.map((user) => (
+          <ListItem key={user.id}>
+            <ListItemText primary={user.name} secondary={user.email} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 }
 
-export default App
+export default App;
